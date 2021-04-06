873f5e8f4b35fab0fa98694ed67bc1359d44f292
Bernardino Sassoli <sassoli@gmail.com>
2021-04-06 11:41:05 +0200

    initial commit

--- culturemap/settings.py
+++ culturemap/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_on_heroku.settings(locals())
