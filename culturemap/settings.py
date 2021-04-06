1806370a3c5e8f9f0c6e0e2a422aa754a8cf678d
Bernardino Sassoli <sassoli@gmail.com>
2021-04-06 12:10:03 +0200

    fixed bug in views.py

--- culturemap/settings.py
+++ culturemap/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_on_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +91,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 # Define AuthUserModel
 AUTH_USER_MODEL = 'venues.User'
