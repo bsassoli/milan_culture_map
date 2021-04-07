41f6bcdf91df7a1b3b095fa1667326113cad74de
Bernardino Sassoli <sassoli@gmail.com>
2021-04-07 19:43:46 +0200

    implemented db connection

--- culturemap/settings.py
+++ culturemap/settings.py
@@ -13,10 +13,15 @@ import os
 from pathlib import Path
 import django_on_heroku
 import dj_database_url
+import dotenv
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,12 +92,17 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 # Use this locally
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+# Use for production
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Define AuthUserModel
 AUTH_USER_MODEL = 'venues.User'
@@ -144,6 +155,12 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+# Uncomment this when deploying
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Uncomment this when deploying
 django_on_heroku.settings(locals())
+
+# Uncomment this when deploying
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
