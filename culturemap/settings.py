0029709f6385cbcec36857a8904fe0a4daf96505
Bernardino Sassoli <sassoli@gmail.com>
2021-04-14 01:54:33 +0200

    modified settings.py

--- culturemap/settings.py
+++ culturemap/settings.py
@@ -32,16 +32,14 @@ TEMPLATE_DIRS = [
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's7bd592!zcpn+$v6#9u1c2yurz^0i_&=_#3!6#3c=tm&+$b%-9'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
-
+ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
