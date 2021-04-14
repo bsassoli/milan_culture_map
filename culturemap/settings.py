d6283ea6f7565f1920b9bd21584ba69261fd0d43
Bernardino Sassoli <sassoli@gmail.com>
2021-04-14 02:07:25 +0200

    modified settings.py

--- culturemap/settings.py
+++ culturemap/settings.py
@@ -35,9 +35,9 @@ TEMPLATE_DIRS = [
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
