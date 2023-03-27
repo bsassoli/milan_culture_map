a041ece3b39f022adc5de379e7d0ecfbeb936ac4
Bernardino Sassoli <sassoli@gmail.com>
2023-03-27 18:44:28 +0200

    modifies culturempa/wsgi.py removing reference to django_blog

--- culturemap/wsgi.py
+++ culturemap/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "culturemap.settings")
 application = get_wsgi_application()
 app = application
